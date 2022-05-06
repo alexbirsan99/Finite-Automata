89f1793b5a2e16f88a8533455d051422423ef217
alexbirsan99 <99223592+alexbirsan99@users.noreply.github.com>
2022-05-06 21:56:17 +0300

    states added and mouse actions

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,68 @@ import React, { useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Sketch from 'react-p5';
+import State from './objects/state';
+import { clear } from 'console';
 
 const App = () => {
+
+  let states: State[] = [];
+
+  let stateSelected: any;
 
   useEffect(() => {
     document.title = 'Finite Automata';
   });
 
-  let setup = (p5:any, parent:any) => {
+  let setup = (p5: any, parent: any) => {
     p5.createCanvas(p5.displayWidth, p5.displayHeight).parent(parent);
+    buildStates(p5);
   }
 
 
-  let draw = (p5:any) => {
-    p5.circle(50,50, 80)
+  let draw = (p5: any) => {
+    states.forEach(element => {
+      if (element.hoverOnState(p5.mouseX, p5.mouseY)) {
+        element.draw('rgb(0,255,255)');
+      } else {
+        element.draw();
+      }
+    });
+  }
+
+  let mousePressed = (p5: any) => {
+    states.forEach(element => {
+      if (element.hoverOnState(p5.mouseX, p5.mouseY)) {
+        stateSelected = element;
+      }
+    });
+  }
+
+  let mouseDragged = (p5:any) => {
+    p5.clear();
+    states.forEach(element => {
+      if (element.hoverOnState(p5.mouseX, p5.mouseY)) {
+        if(element === stateSelected) {
+          element.x = p5.mouseX;
+          element.y = p5.mouseY;
+        }
+      }
+    });
+  }
+
+  let mousedReleased = (p5:any) => {
+    stateSelected = null;
+  }
+
+  let buildStates = (p5: any) => {
+    for (let i = 0; i < 5; i++) {
+      states.push(new State(p5, 200 + (100 * i), 200 + (100 * i), i.toString()));
+    }
   }
 
   return (
     <div>
-      <Sketch setup={setup} draw = {draw} />
+      <Sketch setup={setup} draw={draw} mouseReleased = {mousedReleased} mouseDragged = {mouseDragged} mousePressed={mousePressed} />
     </div>
   );
 }
---
+++ src/objects/state.ts
@@ -0,0 +1,45 @@
+
+
+export default class State {
+
+    diamater:number = 90;
+
+    x:number;
+
+    y:number;
+
+    circle:any;
+
+    stateName:string;
+
+    p5:any;
+
+    constructor(p5:any, x:number, y:number, stateName:string) {
+        this.p5 = p5;
+        this.x = x;
+        this.y = y;
+        this.stateName = stateName;
+    }
+
+    draw(color?:string) {
+        return [
+            color ? [this.p5.stroke(color), this.p5.strokeWeight(3)] : [this.p5.stroke('black'), this.p5.strokeWeight(1)],
+            this.p5.circle(this.x,this.y, this.diamater),
+            this.p5.textSize(32),
+            this.p5.text('q', this.x - 16, this.y + 10),
+            this.p5.textSize(16),
+            this.p5.text(this.stateName, this.x + 4, this.y + 16),
+        ];
+    }
+
+
+    hoverOnState(mouseX:number, mouseY:number) {
+        return (
+            mouseX > this.x - this.diamater / 2 &&
+            mouseX < this.x + this.diamater / 2 &&
+            mouseY > this.y - this.diamater / 2 &&
+            mouseY < this.y + this.diamater / 2 
+        );
+    }
+
+}
